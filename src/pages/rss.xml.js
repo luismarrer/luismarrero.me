fa032886d17f9ac7dfe6922400f90da1c9852282
Luis Marrero <lmarrero1070@gmail.com>
2025-07-14 21:11:02 -0400

    fix: correct variable name for site description in RSS feed generation

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { siteTitle, SiteDescription } from "../data/site.json";
+import { siteTitle, siteDescription } from "@/data/site.json";
 
 export async function GET(context) {
   const posts = await getCollection("posts");
   return rss({
     title: siteTitle,
-    description: SiteDescription,
+    description: siteDescription,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
